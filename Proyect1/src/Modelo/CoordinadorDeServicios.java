57ec2716889d1bbfe45696851d2439cc08cbe4aa
ACarbonellG2 <98650907+ACarbonellG2@users.noreply.github.com>
2023-04-02 13:06:33 -0500

    Add files via upload

---
+++ Proyect1/src/Modelo/CoordinadorDeServicios.java
@@ -0,0 +1,44 @@
+package Modelo;
+
+public class CoordinadorDeServicios {
+	
+	
+	private CatalogoServicios catalogo;
+	
+public CoordinadorDeServicios ()
+	
+	{
+	
+	this.catalogo = catalogo;
+	
+	
+	
+		
+	}
+
+
+public void registrarServicios()
+{
+	return ;
+}
+
+
+public void registrarPagos()
+{
+	
+	
+}
+
+public void mostrarServicios()
+{
+	
+}
+
+
+public Factura generarFactura(Servicio servicio)
+{
+	return null;
+	
+	
+}
+}
