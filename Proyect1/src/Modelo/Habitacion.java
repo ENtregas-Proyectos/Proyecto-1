7ba8cb87d09d2b8817baa7986827d0deffebe700
ACarbonellG2 <98650907+ACarbonellG2@users.noreply.github.com>
2023-04-02 12:11:19 -0600

    Update Habitacion.java

--- Proyect1/src/Modelo/Habitacion.java
+++ Proyect1/src/Modelo/Habitacion.java
@@ -6,36 +6,99 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Habitacion 
-{
-	
+{	
 	private double registroConsumos;
 	private  String nombre;
-	private int numero;
+	private int camas;
 	private String ubicacion;
 	private int capacidad;
+	private ArrayList<String> Tamano;
+	private String tipo;
+	private String Extra; 
+	private String tarifa;
 	
-	public Habitacion(String nombre, int capacidad, int numero, String ubicacion)
+	public Habitacion (String nombre,String ubicacion, int capacidad, int camas, ArrayList<String> Tamano, String tipo, String Extra, String tarifa)
 	{
 		this.nombre = nombre;
-		this.capacidad=capacidad;
-		this.numero = numero;
 		this.ubicacion = ubicacion;
-		
+		this.capacidad=capacidad;
+		this.camas = camas;
+		this.Tamano=Tamano;
+		this.tipo=tipo;
+		this.Extra=Extra;
+		this.tarifa=tarifa;
 	}
 	
-	public int getNumero()
+	
+	public String getNumero()
 	{
-		return numero;
+		return nombre;
 	}
 	
 	public int getCapacidad()
 	{
 		return capacidad;
 	}
+	public int getCamas()
+	{
+		return camas;
+	}
+	public ArrayList<String> getTamano(){
+		return Tamano; 
+	}
 	
 	public String getUbicacion()
 	{
 		return ubicacion;
+	}
+	public String getTipo()
+	{
+		return tipo;
+	}
+	public String getExtra()
+	{
+		return Extra;
+	}
+	public String getTarifa() {
+		return tarifa;
+	}
+	public String getTamanoString() {
+		String tamanos="";
+		if (Tamano.size()>1) {
+			for (int a =0; a< Tamano.size();a++) {
+			tamanos+= Tamano.get(a)+",";
+			}
+		}
+		else {
+			tamanos= Tamano.get(0);
+		}
+		
+		return tamanos;
+	}
+	
+	public void setTarifa(String nuevo) {
+		tarifa=nuevo;
+	}
+	public void setnombre(String nuevo) {
+		nombre=nuevo;
+	}
+	public void setcapacidad(int nuevo) {
+		capacidad=nuevo;
+	}
+	public void setubicacion(String nuevo) {
+		ubicacion=nuevo;
+	}
+	public void setCamas(int nuevo) {
+		camas=nuevo;
+	}
+	public void setTamano(ArrayList<String> nuevo) {
+		Tamano=nuevo;
+	}
+	public void setTipo(String nuevo) {
+		tipo=nuevo;
+	}
+	public void setExtra(String nuevo) {
+		Extra=nuevo;
 	}
 	
 	private double getConsumos()
